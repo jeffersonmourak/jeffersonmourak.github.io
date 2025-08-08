5e7a2832c7b19cabfed57ffacf4cbee016819500
Jefferson Oliveira <jeffersonmourak@gmail.com>
2025-08-08 10:23:28 -0300

    testing openreplay

--- assets/ts/highlight.ts
+++ assets/ts/highlight.ts
@@ -1,9 +1,19 @@
 import { H } from "highlight.run";
 import type { HighlightPublicInterface } from "highlight.run/dist/client/src";
 
+import Tracker from "@openreplay/tracker";
+import trackerAssist from "@openreplay/tracker-assist";
+
 const PROJECT_ID = "0dq3no4e";
 
-((H: HighlightPublicInterface) => {
+const trackerInstance = new Tracker({
+  projectKey: "4HeENhaRGWlCyfPsCLvO",
+});
+
+((H: HighlightPublicInterface, tracker: Tracker) => {
+  tracker.use(trackerAssist());
+  tracker.start();
+
   H.init(PROJECT_ID, {
     environment: "production",
     version: "commit:abcdefg12345",
@@ -20,4 +30,4 @@ const PROJECT_ID = "0dq3no4e";
   window.addEventListener("load", () => {
     H.track("pageview", { pathname: location.pathname });
   });
-})(H);
+})(H, trackerInstance);
