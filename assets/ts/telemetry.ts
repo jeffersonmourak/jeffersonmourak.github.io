6096dbf8c67684cb40f85f5e72a7e97a1a00e712
Jefferson Oliveira <jeffersonmourak@gmail.com>
2025-09-24 10:28:23 -0300

    Removed old trackers

---
+++ assets/ts/telemetry.ts
@@ -0,0 +1,27 @@
+import Tracker from "@openreplay/tracker";
+import sillyname from "sillyname";
+import posthog from 'posthog-js'
+
+const trackerInstance = new Tracker({
+  projectKey: "4HeENhaRGWlCyfPsCLvO",
+});
+
+(() => {
+  const currentUserId =
+    localStorage.getItem("com.jeffersonmourak.id") ?? sillyname();
+
+  posthog.init('phc_XI7kmHnCjoI2ggT8i1oYnXvLV05kVvMAJCjXtt5qdW6',
+    {
+      api_host: 'https://us.i.posthog.com',
+      person_profiles: 'identified_only' // or 'always' to create profiles for anonymous users as well
+    }
+  )
+
+
+
+  window.addEventListener("load", () => {
+    posthog.identify(currentUserId);
+    posthog.capture("pageview", { pathname: location.pathname });
+    localStorage.setItem("com.jeffersonmourak.id", currentUserId);
+  });
+})();
