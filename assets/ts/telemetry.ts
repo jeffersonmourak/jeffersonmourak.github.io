087fda8b94c54227b337e9ccd64b4e2da499d61e
Jefferson Oliveira <jeffersonmourak@gmail.com>
2026-03-12 16:35:09 -0300

    Updated telemetry settings

--- assets/ts/telemetry.ts
+++ assets/ts/telemetry.ts
@@ -7,8 +7,8 @@ import posthog from 'posthog-js'
 
   posthog.init('phc_XI7kmHnCjoI2ggT8i1oYnXvLV05kVvMAJCjXtt5qdW6',
     {
-      api_host: 'https://us.i.posthog.com',
-      person_profiles: 'identified_only' // or 'always' to create profiles for anonymous users as well
+      api_host: 'https://t.jeffersonmourak.com',
+      person_profiles: 'always' // or 'always' to create profiles for anonymous users as well
     }
   )
 
